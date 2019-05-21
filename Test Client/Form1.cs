2893ce2b8f456635488f8ba1740305789153477a
Jesús Alejando Noguera Ballén <jnoguera@diit.com.co>
2019-05-21 16:48:05 -0500

    Add project files.

---
+++ Test Client/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_Client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.ServiceClient myService = new
+                    ServiceReference1.ServiceClient();
+            MessageBox.Show(myService.GetData(123));
+            myService.Close();
+
+        }
+    }
+}
